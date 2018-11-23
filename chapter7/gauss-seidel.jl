1d73a088dc2c3446bc0102bd0662baf23eedf5a9
Matteo Semenzato <mattew8898@gmail.com>
2018-11-23 12:20:33 +0100

    Added Gauss-Seidel iteration.

--- chapter7/gauss-seidel.jl
+++ chapter7/gauss-seidel.jl
@@ -1,21 +1,27 @@
-# Algorithm 7.1
-# Jacobi Iteration
+# Algorithm 7.2
+# Gauss-Seidel Iteration
 
 using LinearAlgebra
 
-function jacobi(mat, approx, known, maxIter, tol)
+function gaussseidel(mat, approx, known, maxIter, tol)
 	n, m = size(mat)
 	sol = Vector(undef, n)
 	a = mat
 	for k in 1:maxIter
 		for i in 1:n
-			sum = 0
-			for j in 1:n
+			sum1 = 0
+			for j in 1:(i - 1)
 				if j != i
-					sum += (a[i, j]*approx[j]) + known[i]
+					sum1 += (a[i, j]*x[j])
 				end
 			end
-			sol[i] = (-sum)/a[i, i]
+			sum2 = 0
+			for j in (i + 1):n
+				if j != i
+					sum2 += (a[i, j]*approx[j]) + known[i]
+				end
+			end
+			sol[i] = (-sum1 - sum2)/a[i, i]
 		end
 		if LinearAlgebra.norm(sol - approx) < tol
 			println("Success")
@@ -31,4 +37,4 @@ end
 A = [[1.0 1.33]; [2.0 -2.55];]
 x = [0.0, 0.0]
 b = [2.0, 5.0]
-println(jacobi(A, x, b, 50, 0.01))
+println(gaussseidel(A, x, b, 90, 0.001))
